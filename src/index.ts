0dfb752d01471d07548a8e50a48ea2c8ecedaf95
Ryan L. Bell <ryan.l.bell@gmail.com>
2018-08-08 12:03:00 -0400

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import { err, ok, Result } from 'resulty';
+
+const parseIntR = (value: string): Result<string, number> => {
+  const n = parseInt(value, 10);
+  return isNaN(n) ? err(`Could not convert '${value}' to an integer`) : ok(n);
+};
+
+const add = (x: number) => (y: number): number => x + y;
+
+const asString = (result: Result<any, any>): string =>
+  result.cata({
+    Ok: v => `Ok ${JSON.stringify(v)}`,
+    Err: err => `Err ${JSON.stringify(err)}`,
+  });
+
+const logR = (result: Result<any, any>): void => {
+  console.log(asString(result));
+};
+
+logR(parseIntR('4'));
+
+logR(parseIntR('foo'));
+
+logR(parseIntR('4').map(add(2)));
+
+logR(parseIntR('4').andThen(x => parseIntR('5').map(y => ({ x, y }))).map(nums => nums.x + nums.y));
+
+logR(
+  parseIntR('foo').andThen(x => parseIntR('5').map(y => ({ x, y }))).map(nums => nums.x + nums.y),
+);
+
+logR(
+  parseIntR('4').andThen(x => parseIntR('bar').map(y => ({ x, y }))).map(nums => nums.x + nums.y),
+);
+
+logR(ok(add).ap(parseIntR('4')).ap(parseIntR('5')));
+
+logR(ok(add).ap(parseIntR('foo')).ap(parseIntR('5')));
